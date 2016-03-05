6f142b816e329f0605e0ed318c3a2d11dda36717
Runzhong Zhang <zhangrunzhong1997@gmail.com>
2016-03-04 20:28:09 -0800

    A working AI that picks the first valid move

--- player.cpp
+++ player.cpp
@@ -8,7 +8,8 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
-
+	player_side = side;
+	board = new Board();
     /* 
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
@@ -39,5 +40,37 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */ 
-    return NULL;
+    
+    // Find the opponent's side
+    Side opponent_side;
+    if (player_side == BLACK) {
+		opponent_side = WHITE;
+	}
+	else {
+		opponent_side = BLACK;
+	}
+	
+	// Update the board
+	board->doMove(opponentsMove, opponent_side);
+	
+	// If there is not valid move, return NULL
+    if (board->hasMoves(player_side) == false){
+		return NULL;
+	}
+	
+	// Return the first valid move
+	for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            Move * random_move = new Move(i, j);
+            if (board->checkMove(random_move, player_side)) 
+            {
+				board->doMove(random_move, player_side);
+				return random_move;
+			}
+        }
+    }
+    
+	return NULL;
+	
+    
 }
